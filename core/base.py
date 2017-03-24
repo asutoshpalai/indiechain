08d0c27f469aeb528666370f5ec509dda32ad4e0
meetvora <v.h.c.meetirth@gmail.com>
2017-03-24 16:44:10 +0530

    Core: Update base file

--- core/base.py
+++ core/base.py
@@ -8,20 +8,24 @@ class BlockHeader(object):
 		self.previous_hash = prev_hash
 		self.timestamp = time()
 		self.target_threshold = threshold
+	
 	def Root(block):
 		ids = [transaction for transaction in block.transactions]
 		#calculate merkle root and return
 		pass
+
 	def Nonce():
 		pass
-	def __save__(self, transactions, block):
+	
+	def save(self, transactions, block):
 		self.merkle_root = Root(transactions)
 		self.nonce = Nonce(block)
 		self.size = getSize(block)
 
+
 class Block(object):
 	def __init__(self, threshold = 4):
-		prev_block = network.getHead()
+		prev_block = network.getLocalHead()
 		self.header = BlockHeader(prev_block.hash,threshold)
 		self.flags = 0x00
 		self.transactions = []
@@ -30,15 +34,19 @@ class Block(object):
 	def addTransaction(sender_id, receiver_id, amount):
 		sender = network.getNode(sender_id)
 		receiver = network.getNode(receiver_id)
-		transaction = Transaction(sender_id, receiver_id, amount, self)
-		self.transactions.append(transaction)
+		try:
+			transaction = Transaction(sender_id, receiver_id, amount, self)
+			self.transactions.append(transaction)
+		except TransactionError:
+			return 'Invalid transaction. Try again.'	
 
-	def __save__(self):
+	def save(self):
 		self.reward = 0
 		self.hash = self.Hash(self)
 		self.difficulty = self.Difficulty(self.hash)
 		self.signature = self.generateSignature(network.getCurrent().key())
 		self.miner = network.getMiner(self)
+
 
 class Transaction(object):
 	__slots__ = ['hash', 'block']
@@ -46,20 +54,26 @@ class Transaction(object):
 	def __init__(self, sender_id, receiver_id, amount, block):
 		chain = network.getChain()
 		self.hash = self.Hash(sender_id, receiver_id, amount)
-		if (self.verify(self)):
+		if (self.verify(sender_id, receiver_id, amount)):
 			self.block = block
 			chain.transactions.append(self.hash)
+		else:
+			raise TransactionError
 	
 	def __repr__(self):
 		return "<%s> * %s *" %(self.block, self.hash[:10])
 
-	def verify(self):
+	def verify(sender_id, receiver_id, amount):
+		sender = network.getNode(sender_id)
+		receiver = network.getNode(receiver_id)
 		if transaction_valid:
 			return True
-		raise TransactionError("Invalid transaction.")
+		return False
+
 
 class indieChain(object):
 	pass
+
 
 class TransactionError(Exception):
 	pass
